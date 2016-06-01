3ae16d03dacd0c2260be85e774f8b5f729c658b6
Victor <vincenzogreggio@hotmail.com>
2016-06-01 06:31:37 -0500

    Probando File

---
+++ Prueba/LibreriaProbar/LibreriaProbar.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Prueba;
+using System.IO;
+
+namespace LibreriaProbar
+{
+    [TestFixture]
+    public class LibreriaProbar
+    {
+        //Archivo
+        [Test]
+        public void ArchivoStringEqual()
+        {
+            Probar objPrueba = new Probar();
+            string read = File.ReadAllText("Archivo.txt", Encoding.UTF8);
+            Assert.IsTrue(objPrueba.CompararIguales(read,"Hola"));
+        }
+        //Valores Iguales
+        [Test]
+        public void TestSumaEqual()
+        {
+            Probar objPrueba = new Probar();
+            Assert.AreEqual(9, objPrueba.Suma(8, 1));
+        }
+
+        [Test]
+        public void TestUnirEqual()
+        {
+            Probar objPrueba = new Probar();
+            Assert.AreEqual("Don Ian", objPrueba.Unir("Don ", "Ian"));
+        }
+
+
+        //Valores Distintos
+        [Test]
+        public void TestSumaNotEqual()
+        {
+            Probar objPrueba = new Probar();
+            Assert.AreNotEqual(9, objPrueba.Suma(1, 2));
+        }
+
+        [Test]
+        public void TestUnirNotEqual()
+        {
+            Probar objPrueba = new Probar();
+            Assert.AreNotEqual("Don Ian", objPrueba.Unir("No", "Ian"));
+        }
+
+        //Valores Nulos
+        [Test]
+        public void TestUnirNull()
+        {
+            Probar objPrueba = new Probar();
+            Assert.IsNull(objPrueba.Unir(null, null));
+        }
+
+        //Boolean
+        [Test]
+        public void TestBoolTrue()
+        {
+            Probar objPrueba = new Probar();
+            Assert.IsTrue(objPrueba.CompararIguales("Igual", "Igual"));
+        }
+
+        [Test]
+        public void TestBoolFalse()
+        {
+            Probar objPrueba = new Probar();
+            Assert.IsFalse(objPrueba.CompararIguales("Igual", "Igul"));
+        }
+    }
+}
